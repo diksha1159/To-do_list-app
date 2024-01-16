2fd043fa3c6b520487d78ab1dc4fcde90407e539
Diksha <76660222+diksha1159@users.noreply.github.com>
2024-01-16 16:48:23 +0000

    made the todo box dragagble, adding that aesthetic vibe 💅

--- script.js
+++ script.js
@@ -1,71 +1,88 @@
 let inputBox = document.getElementById("text");
 let itemsBox = document.getElementById("items");
+let container = document.querySelector(".container");
 
 inputBox.addEventListener("keyup", (e) => {
-  if (e.key == "Enter") {
-    let value = inputBox.value;
-    let toDos = localStorage.getItem("todos");
-    if (toDos == null) {
-      todoObj = [];
-    } else {
-      todoObj = JSON.parse(toDos);
+    if (e.key == "Enter") {
+        let value = inputBox.value;
+        let toDos = localStorage.getItem("todos");
+        if (toDos == null) {
+            todoObj = [];
+        } else {
+            todoObj = JSON.parse(toDos);
+        }
+        todoInfo = { name: value, status: "pending" };
+        todoObj.push(todoInfo);
+        localStorage.setItem("todos", JSON.stringify(todoObj));
+        inputBox.value = "";
+        showTodos();
     }
-    todoInfo = { name: value, status: "pending" };
-    todoObj.push(todoInfo);
-    localStorage.setItem("todos", JSON.stringify(todoObj));
-    inputBox.value = "";
-    showTodos();
-  }
 });
 
 function showTodos() {
-  let toDos = localStorage.getItem("todos");
-  if (toDos == null) {
-    todoObj = [];
-  } else {
-    todoObj = JSON.parse(toDos);
-  }
-
-  let html = "";
-  todoObj.forEach((element, index) => {
-    if (element.status == "done") {
-      html += `<div class="item">
-      <input type="checkbox" class="check-box" onclick="updateStatus(this)" id=${index} checked>
-      <h3 class="taskDetail done">${element.name}</h3>
-      <i class="fa-solid fa-trash options" onclick="deleteTodo(${index})"></i>
-      </div>`;
+    let toDos = localStorage.getItem("todos");
+    if (toDos == null) {
+        todoObj = [];
     } else {
-      html += `<div class="item">
-      <input type="checkbox" class="check-box" onclick="updateStatus(this)" id=${index}>
-      <h3 class="taskDetail">${element.name}</h3>
-      <i class="fa-solid fa-trash options" onclick="deleteTodo(${index})"></i>
-      </div>`;
+        todoObj = JSON.parse(toDos);
     }
-  });
-  itemsBox.innerHTML = html;
+
+    let html = "";
+    todoObj.forEach((element, index) => {
+        if (element.status == "done") {
+            html += `<div class="item">
+            <input type="checkbox" class="check-box" onclick="updateStatus(this)" id=${index} checked>
+            <h3 class="taskDetail done">${element.name}</h3>
+            <i class="fa-solid fa-trash options" onclick="deleteTodo(${index})"></i>
+            </div>`;
+        } else {
+            html += `<div class="item">
+            <input type="checkbox" class="check-box" onclick="updateStatus(this)" id=${index}>
+            <h3 class="taskDetail">${element.name}</h3>
+            <i class="fa-solid fa-trash options" onclick="deleteTodo(${index})"></i>
+            </div>`;
+        }
+    });
+    itemsBox.innerHTML = html;
 }
 
 function updateStatus(selectedTodo) {
-  // console.log(selectedTodo);
-  let h3 = selectedTodo.parentElement.lastElementChild;
-  let toDos = JSON.parse(localStorage.getItem("todos"));
-  if (selectedTodo.checked) {
-    h3.classList.add("done");
-    toDos[selectedTodo.id].status = "done";
-  } else {
-    h3.classList.remove("done");
-    toDos[selectedTodo.id].status = "pending";
-  }
-  localStorage.setItem("todos", JSON.stringify(toDos));
-  showTodos();
+    let h3 = selectedTodo.parentElement.lastElementChild;
+    let toDos = JSON.parse(localStorage.getItem("todos"));
+    if (selectedTodo.checked) {
+        h3.classList.add("done");
+        toDos[selectedTodo.id].status = "done";
+    } else {
+        h3.classList.remove("done");
+        toDos[selectedTodo.id].status = "pending";
+    }
+    localStorage.setItem("todos", JSON.stringify(toDos));
+    showTodos();
 }
 
 function deleteTodo(id) {
-  // console.log(id);
-  let toDos = JSON.parse(localStorage.getItem("todos"));
-  toDos.splice(id, 1);
-  localStorage.setItem("todos", JSON.stringify(toDos));
-  showTodos();
+    let toDos = JSON.parse(localStorage.getItem("todos"));
+    toDos.splice(id, 1);
+    localStorage.setItem("todos", JSON.stringify(toDos));
+    showTodos();
 }
+
+container.addEventListener("mousedown", (e) => {
+    let offsetX = e.clientX - container.getBoundingClientRect().left;
+    let offsetY = e.clientY - container.getBoundingClientRect().top;
+
+    function moveContainer(event) {
+        container.style.left = event.clientX - offsetX + "px";
+        container.style.top = event.clientY - offsetY + "px";
+    }
+
+    function stopMoving() {
+        document.removeEventListener("mousemove", moveContainer);
+        document.removeEventListener("mouseup", stopMoving);
+    }
+
+    document.addEventListener("mousemove", moveContainer);
+    document.addEventListener("mouseup", stopMoving);
+});
 
 showTodos();
